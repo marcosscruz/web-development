e5cc10c3381d72f1c02e9e6a3f7f3d9fae7a1415
Marcos <125505999+marcosscruz@users.noreply.github.com>
2023-02-27 12:42:20 -0300

    fundamentos

---
+++ Fundamentos/inicio/dados_1.js
@@ -0,0 +1,16 @@
+// pode declarar uma variavel tanto com 'var' como com 'let'
+
+let nome = 'Caneta';
+var quantidade = 10;
+var preco = 6.5;
+var imposto = 1.3;
+var precoFinal = preco + imposto;
+
+console.log(nome)
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = 'Centa BIC';
+console.log(nome)
