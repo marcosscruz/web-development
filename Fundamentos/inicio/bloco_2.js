9e311dd6a0e81658c0e58c505950ef7909ce1879
Marcos <125505999+marcosscruz@users.noreply.github.com>
2023-02-27 17:17:21 -0300

    fundamentos

---
+++ Fundamentos/inicio/bloco_2.js
@@ -0,0 +1,11 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.9 * 0.6);
+let precoCOmDesconto = (preco * (1 - desconto));
+
+console.log(precoCOmDesconto);
+
+let nome = 'caderno';
+let categoria = "papelaria";
+console.log("Produto: " + nome + ", Categoria: " + categoria);
